93668c43f8c175aa48fb6483531bf8d7056fa9ff
Raghav Subbaraman <rsubbaraman@eng.ucsd.edu>
2023-09-10 22:30:24 -0700

    bugfixes, testing on wcsng27

--- loratestbed/main_controller.py
+++ loratestbed/main_controller.py
@@ -8,7 +8,7 @@ from loratestbed.device_manager import DeviceManager
 def main():
     logging.basicConfig(
         format="[%(asctime)s] [%(levelname)s] %(message)s",
-        level=logging.DEBUG,
+        level=logging.INFO,
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
@@ -18,16 +18,8 @@ def main():
     PORT = args["port"]
     interface = SerialInterface(PORT)
 
-    # # Create a message 1, 33, 0,0,0
-    # message = b"\x01\x21\x00\x00\x00"
-
-    # # read_bytes = interface._write_read_bytes(b"\x00\x00\x00\x00\x00")
-    # logging.info(f"Writing {message} to serial port")
-    # read_bytes = interface._write_read_bytes(message)
-
-    # logging.info(f"Read {read_bytes} from serial port")
-    device_manager = DeviceManager([33], interface)
-    device_manager._ping_devices(33)
+    device_manager = DeviceManager([26, 33], interface)
+    # device_manager._ping_devices(33)
 
 
 if __name__ == "__main__":
